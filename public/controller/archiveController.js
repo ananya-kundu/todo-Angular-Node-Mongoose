759369b37ca8e97b62cdc23d84e49e00137a892d
Ananya <ananyakundu.gnit@gmail.com>
2017-06-08 19:42:48 +0530

    updated on 8th June

--- public/controller/archiveController.js
+++ public/controller/archiveController.js
@@ -6,11 +6,14 @@ app.controller('archiveController', ['$scope', '$controller', function($scope, $
   $scope.archivesidebar = {'background':"transparent"};
   $scope.archivenav = {'background-color':"rgb(96, 125, 139)"};
   $scope.archivegly = {'color':'white'};
-  $scope.gridstyle = {'color':'white'};
   $scope.liststyle = {'color':'white'};
+  $scope.gridstyle = {'color':'white'};
   $scope.archivebrand = {'color':'white'};
   $scope. archivesearch = {'background-color':"rgb(96, 125, 139)"};
-  $scope. archivequery = {'background-color':"rgb(122, 146, 158);"};
+  $scope. archivequery = {'background-color':"rgba(122, 146, 158,1)",'color':'white'};
+    $scope. archivequerybtn = {'background-color':"rgba(122, 146, 158,1)"};
+  // $scope. archivequery = {'background-color':"rgba(0.232,232,0.04);"};
+
 
   console.log("archive",$scope.archivenote);
 }]);
